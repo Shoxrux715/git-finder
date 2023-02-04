47af29a2f806dc9f8da89fab9db8a425c9a96554
walker <walker@gmail.com>
2023-02-04 22:29:53 +0500

    git

---
+++ src/components/Main.vue
@@ -0,0 +1,31 @@
+<template>
+  <main class="main">
+   <div class="container">
+      <Search/>
+      <h2 class="error" v-if="error">{{error}}</h2>
+      <User/>
+      <Repost/>
+   </div>
+  </main>
+</template>
+
+<script>
+import Search from "@/components/Search.vue";
+import User from "@/components/User.vue";
+import Repost from "@/components/Repost.vue";
+import { mapState } from "vuex";
+export default {
+components:{
+   Search,
+   User,
+   Repost
+},
+computed:{
+   ...mapState(['error'])
+}
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Repost.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="repos">
+    <Repositem 
+    v-for="repos in getReposSort" 
+    :key="repos.id"
+    :repos="repos"
+    />
+  </div>
+</template>
+
+<script>
+import Repositem from "@/components/Repositem.vue";
+import { mapGetters } from "vuex";
+export default {
+  components: {
+    Repositem,
+  },
+  computed:{
+    ...mapGetters(['getReposSort'])
+  }
+};
+</script>
+
+<style>
+</style>
